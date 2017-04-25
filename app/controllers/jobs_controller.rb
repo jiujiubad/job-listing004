dba72267746216eee88b29e23630228090c8d80c
谢育初 <524945638@qq.com>
2017-04-25 15:08:26 +0800

    add serch function 戴键林师兄

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,7 @@
 class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
-
+  before_action :validate_search_key, only: [:search]
+  
   def index
     @jobs = case params[:order]
     when 'by_lower_bound'
@@ -52,10 +53,26 @@ class JobsController < ApplicationController
     redirect_to jobs_path, alert: "Job deleted"
   end
 
+  def search
+    if @query_string.present?
+      search_result = Job.published.ransack(@search_criteria).result(:distinct => true)
+        @jobs = search_result.paginate(:page => params[:page], :per_page => 5 )
+    end
+  end
+
   private
 
   def job_params
     params.require(:job).permit(:title, :description, :wage_lower_bound, :wage_upper_bound, :contact_email, :is_hidden)
+  end
+
+  def validate_search_key
+    @query_string = params[:q].gsub(/\\|\'|\/|\?/, "") if params[:q].present?
+    @search_criteria = search_criteria(@query_string)
+  end
+
+  def search_criteria(query_string)
+    { :title_cont => query_string }
   end
 
 end
